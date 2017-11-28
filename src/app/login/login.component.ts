1536cc9f5fdabda0ceb8960951dbff1e1bef5bcc
Daniel J. Slapelis <djs7209@rit.edu>
2017-11-28 02:23:11 -0500

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: any = {};
+  returnUrl: string;  
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.returnUrl = 'dashboard';    
+  }
+
+  login() {
+    this.authenticationService.login(this.model.email, this.model.password)
+      .subscribe(data => {
+        this.router.navigate([this.returnUrl]);
+      },
+      error => {});
   }
 
 }

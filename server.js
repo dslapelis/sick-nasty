22e6a8733715a45066ed77dad38ab5a5a8fe5bf9
Daniel J. Slapelis <djs7209@rit.edu>
2017-11-28 04:27:26 -0500

    port 80

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '80';
 app.set('port', port);
 
 /**
